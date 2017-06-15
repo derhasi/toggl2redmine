46deae778b0ea509bb8673b8861e59145dcfd1ec
Johannes Haseitl <johannes@undpaul.de>
2017-06-15 21:56:46 +0200

    Update docblocks

--- src/derhasi/toggl2redmine/TimeEntryCollection.php
+++ src/derhasi/toggl2redmine/TimeEntryCollection.php
@@ -14,8 +14,14 @@ class TimeEntryCollection implements \Countable {
    */
   protected $entries = [];
 
+  /**
+   * @var array
+   */
   protected $sync = [];
 
+  /**
+   * @var array
+   */
   protected $redmineEntries = [];
 
   /**
@@ -50,6 +56,10 @@ class TimeEntryCollection implements \Countable {
     return count($this->entries);
   }
 
+  /**
+   * Provide redmine entries to associate with the given list of toggl entries.
+   * @param $redmineEntries
+   */
   public function processRedmineEntries($redmineEntries) {
     $this->redmineEntries = [];
     $combinations = [];
@@ -80,20 +90,40 @@ class TimeEntryCollection implements \Countable {
       // Only use this combination, when both toggl and redmine entry to do not
       // have a sync assocation yet.
       if (!$this->togglHasSync($comb['toggl']) && !$this->redmineHasSync($comb['redmine'])) {
-        $this->markSynced($comb['toggl'], $comb['redmine']);
+        $this->associateEntryIDs($comb['toggl'], $comb['redmine']);
       }
     }
   }
 
+  /**
+   * Checks if the toggle time entry has an asociated redmine entry.
+   *
+   * @param int $toggleID
+   *
+   * @return bool
+   */
   protected function togglHasSync($toggleID) {
     return isset($this->sync[$toggleID]);
   }
 
+  /**
+   * Checks if redmine time entry has an associated toggl entry.
+   *
+   * @param int $redmineID
+   *
+   * @return bool
+   */
   protected function redmineHasSync($redmineID) {
     return array_search($redmineID, $this->sync) !== FALSE;
   }
 
-  protected function markSynced($toggleID, $redmineID) {
+  /**
+   * Associate the sync of the two IDs.
+   *
+   * @param int $toggleID
+   * @param int $redmineID
+   */
+  protected function associateEntryIDs($toggleID, $redmineID) {
     $this->sync[$toggleID] = $redmineID;
     $this->entries[$toggleID]->setRedmineEntry($redmineID, $this->redmineEntries[$redmineID]);
   }
